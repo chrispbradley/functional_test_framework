72ac34d3051c413b14011b21805b86508f11beb8
Hugh Sorby <h.sorby@auckland.ac.nz>
2017-11-01 12:14:10 +1300

    Add a check for the existence of expected output.

--- testsetup/run_test.cmake
+++ testsetup/run_test.cmake
@@ -12,6 +12,7 @@ if (NOT TEST_OUTPUT)
     message( FATAL_ERROR "Variable TEST_OUTPUT not defined" )
 endif ()
 
+set(EXPECTED_OUTPUT_FOUND TRUE)
 if (IS_DIRECTORY "${EXPECTED_OUTPUT}")
     set(_NDIFF_COUNT 0)
     # Assume all files inside expected output directory are to be compared.
@@ -21,9 +22,11 @@ if (IS_DIRECTORY "${EXPECTED_OUTPUT}")
         get_filename_component(_output_file ${_file} NAME)
         set(NDIFF_COMPARISON_${_NDIFF_COUNT} COMMAND ${NDIFF_EXECUTABLE} -relerr ${TEST_TOLERANCE} ${_file} ${TEST_OUTPUT}/${_output_file})
     endforeach()
-else ()
+elseif (EXISTS "${EXPECTED_OUTPUT}")
     set(_NDIFF_COUNT 1)
     set(NDIFF_COMPARISON_1 COMMAND ${NDIFF_EXECUTABLE} ${EXPECTED_OUTPUT} ${TEST_OUTPUT})
+else ()
+    set(EXPECTED_OUTPUT_FOUND FALSE)
 endif ()
 
 execute_process(
@@ -33,6 +36,8 @@ execute_process(
 
 if (test_execution_not_successful)
     message(SEND_ERROR "${TEST_NAME} did not execute succesfully!")
+elseif (_NDIFF_COUNT EQUAL 0 OR NOT EXPECTED_OUTPUT_FOUND)
+    message(SEND_ERROR "${TEST_NAME} does not have expected output!")
 else ()
     set(_NDIFF_INDEX 0)
     while(_NDIFF_INDEX LESS _NDIFF_COUNT)
