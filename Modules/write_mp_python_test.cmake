89c6ff920463dedf061cb12e4f7d61f5127f4747
Hugh Sorby <h.sorby@auckland.ac.nz>
2017-11-02 14:36:49 +1300

    Final stage in adding Python test support.

--- Modules/write_mp_python_test.cmake
+++ Modules/write_mp_python_test.cmake
@@ -1,3 +1,6 @@
 
 function(write_test)
+    write_mp_python_test(${SETUP_CMAKELISTS_FILE} ${_current_name}
+        ${_current_target} ${_current_np} ${_current_root} ${_current_expected_results}
+        ${_current_tolerance} ${_current_args})
 endfunction()
