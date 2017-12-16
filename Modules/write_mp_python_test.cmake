958a014ff158b87442b99cfeb4bc7b19eec38cad
Chris Bradley <c.bradley@auckland.ac.nz>
2017-12-17 09:18:14 +1300

    Move tolerance to rel_tolerance and add in abs_tolerance.

--- Modules/write_mp_python_test.cmake
+++ Modules/write_mp_python_test.cmake
@@ -2,5 +2,5 @@
 function(write_test)
     write_mp_python_test(${SETUP_CMAKELISTS_FILE} ${_current_name}
         ${_current_target} ${_current_np} ${_current_root} ${_current_expected_results}
-        ${_current_tolerance} ${_current_args})
+        ${_current_abs_tolerance} ${_current_rel_tolerance} ${_current_args})
 endfunction()
